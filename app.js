7bb136df160f8e1f3364d5287d8190f26f2ebe36
Twinkle Goyal <twinkle.goyal@intimetec.com>
2022-08-10 13:30:06 +0530

    modify mail transport details

--- app.js
+++ app.js
@@ -31,30 +31,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
+  console.log('req.body :', req.body);
   const output = `
-    <p>You have new contact request</p>
-    <h3>Contact Details :</h3>
-    <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Company: ${req.body.company}</li>
-        <li>Email: ${req.body.email}</li>
-        <li>Phone: ${req.body.phone}</li>
-    </ul>
     <h3>Message</h3>
     <p>${req.body.message}</p>
     `;
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'mail.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
-      user: '*****@****.***', // generated ethereal user
-      pass: '***********', // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false,
+      user: 'twinklegoyal786@gmail.com', // generated ethereal user
+      pass: 'gfhiqquegrpmxbox', // generated ethereal password
     },
   });
 
@@ -75,7 +63,7 @@ app.post('/send', (req, res) => {
     console.log('Message sent: %s', info.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-    res.render('contact', { msg: 'Email has been sent' });
+    return res.render('contact', { msg: 'Email has been sent' });
   });
 });
 
